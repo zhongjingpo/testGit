27e9635771c2efafe5b737966d6928ffa1827e5f
zhongjingpo <994095968@qq.com>
2024-05-18 23:39:55 +0800

    第二次修改

--- a.js
+++ a.js
@@ -1,5 +1,7 @@
 const test = () => {
-    console.log('first', 'first push')
+    console.log("first", "first push");
 
-    console.log('feature123', 'add123')
-}
+    console.log("feature123", "add123");
+
+    console.log("second", "second");
+};
